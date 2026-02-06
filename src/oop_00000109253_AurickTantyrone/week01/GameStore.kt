98640e32dc2ecc31925f12a1807acbc3fe8596a4
Kcirua <auricktan@gmail.com>
2026-02-06 23:29:27 +0700

    week01: (tugas 2) implement discount logic

--- src/oop_00000109253_AurickTantyrone/week01/GameStore.kt
+++ src/oop_00000109253_AurickTantyrone/week01/GameStore.kt
@@ -2,5 +2,13 @@ package oop_00000109253_AurickTantyrone.week01
 
 fun main() {
     val gameTitle = "SteamKW"
-    val price = 10000
+    val price = 1000000
+
+    println(calculateDiscount(price))
+}
+
+fun calculateDiscount(price: Int): Int = if (price > 500000) {
+    (price * 0.8).toInt()
+} else {
+    (price * 0.9).toInt()
 }
