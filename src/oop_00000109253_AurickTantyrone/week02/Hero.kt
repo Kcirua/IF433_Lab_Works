8f6f7075bde6e59a34b0530bf94af110bf2b4efe
Kcirua <auricktan@gmail.com>
2026-02-13 23:26:45 +0700

    week02: (task 2) create Hero class

---
+++ src/oop_00000109253_AurickTantyrone/week02/Hero.kt
@@ -0,0 +1,22 @@
+package oop_00000109253_AurickTantyrone.week02
+
+class Hero(
+    val name: String,
+    val baseDamage: Int,
+    var hp: Int = 100
+) {
+    fun attack(targetName: String) {
+        println("$name menyerang $targetName dengan garang!")
+    }
+
+    fun takeDamage(damage: Int) {
+        hp -= damage
+        if (hp < 0) {
+            hp = 0
+        }
+    }
+
+    fun isAlive(): Boolean {
+        return hp > 0
+    }
+}
