cb574534decc0ee67eba5db25f057d9d5db475d0
Kcirua <auricktan@gmail.com>
2026-02-13 22:42:34 +0700

    week02: (task 1) create Loan class with default args

---
+++ src/oop_00000109253_AurickTantyrone/week02/Loan.kt
@@ -0,0 +1,15 @@
+package oop_00000109253_AurickTantyrone.week02
+
+class Loan(
+    var bookTitle: String,
+    var borrower: String,
+    var loanDuration: Int = 1
+) {
+    fun calculateFine(loanDuration: Int): Int {
+        return if (loanDuration > 3) {
+            (loanDuration - 3) * 2000
+        } else {
+            0
+        }
+    }
+}
