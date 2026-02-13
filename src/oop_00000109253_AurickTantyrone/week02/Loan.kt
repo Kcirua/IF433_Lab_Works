73984a283ce872985e27b6763b21210d860e658f
Kcirua <auricktan@gmail.com>
2026-02-13 22:52:11 +0700

    week02: (task 1) implement library input system

--- src/oop_00000109253_AurickTantyrone/week02/Loan.kt
+++ src/oop_00000109253_AurickTantyrone/week02/Loan.kt
@@ -5,7 +5,7 @@ class Loan(
     var borrower: String,
     var loanDuration: Int = 1
 ) {
-    fun calculateFine(loanDuration: Int): Int {
+    fun calculateFine(): Int {
         return if (loanDuration > 3) {
             (loanDuration - 3) * 2000
         } else {
