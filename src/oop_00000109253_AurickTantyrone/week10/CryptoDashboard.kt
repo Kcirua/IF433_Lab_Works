e7a12d76c863c4b9a0a28e9d7b584434d586cd59
Kcirua <auricktan@gmail.com>
2026-05-14 21:53:51 +0700

    week10: (task) populate coin repository

--- src/oop_00000109253_AurickTantyrone/week10/CryptoDashboard.kt
+++ src/oop_00000109253_AurickTantyrone/week10/CryptoDashboard.kt
@@ -3,5 +3,9 @@ package oop_00000109253_AurickTantyrone.week10
 fun main() {
     val coinRepo = WalletRepository<Coin>()
 
+    coinRepo.add(Coin("BTC", 0.5))
+    coinRepo.add(Coin("ETH", 2.0))
+    coinRepo.add(Coin("USDT", 1000.0))
+
     println("=== Crypto Wallet Dashboard Initialized ===")
 }
