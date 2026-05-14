ebe1a69f54c0554b08165118ec39aca340b0d4b5
Kcirua <auricktan@gmail.com>
2026-05-14 22:02:46 +0700

    week10: (task) initialize and populate transaction repository

--- src/oop_00000109253_AurickTantyrone/week10/CryptoDashboard.kt
+++ src/oop_00000109253_AurickTantyrone/week10/CryptoDashboard.kt
@@ -15,4 +15,12 @@ fun main() {
     response.data.forEach { coin ->
         println("Coin Name: ${coin.name} | Balance: ${coin.balance}")
     }
+
+    val txRepo = WalletRepository<Transaction>()
+
+    txRepo.add(Transaction("TXN001", 0.05))
+    txRepo.add(Transaction("TXN002", 1.5))
+    txRepo.add(Transaction("TXN003", 250.0))
+
+    println("Berhasil menambahkan ${txRepo.getAll().size} transaksi ke repositori.")
 }
