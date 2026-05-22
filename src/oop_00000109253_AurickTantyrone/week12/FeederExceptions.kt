f92ad63b60da0b5d7cf243d7907db1ab476b34d3
Kcirua <auricktan@gmail.com>
2026-05-22 12:31:11 +0700

    week12: (task) create custom feeder exception subclasses

--- src/oop_00000109253_AurickTantyrone/week12/FeederExceptions.kt
+++ src/oop_00000109253_AurickTantyrone/week12/FeederExceptions.kt
@@ -1,7 +1,10 @@
 package oop_00000109253_AurickTantyrone.week12
 
 sealed class FeederException(msg: String) : Exception(msg) {
-    class EmptyFoodStockException(msg: String = "Peringatan: Stok makanan kibble habis!") : FeederException(msg)
 
-    class MotorJammedException(msg: String = "Error Mekanik: Motor dispenser tersangkut!") : FeederException(msg)
+    class FoodEmptyException(requested: Int, available: Int) :
+        FeederException("Kibble tidak cukup! Diminta $requested gr, sisa $available gr")
+
+    class DispenserJamException :
+        FeederException("Wadah dispenser tersangkut/macet!")
 }
